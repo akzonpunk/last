beda64f9b44617e44d026ab44b60e04a42b0c2e1
Alumnos <Alumnos@LC01-PC06>
2016-01-14 12:59:29 -0500

    work out 2

---
+++ GOODNIGTH/src/pe/edu/upeu/poo/clase2/repaso.java
@@ -0,0 +1,36 @@
+package pe.edu.upeu.poo.clase2;
+
+import java.util.Scanner;
+
+public class repaso {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		int p = 120;
+		double t = 1.74;
+        repaso  life = new repaso();
+		// ¨¨suma
+		// *resta
+		//
+
+		System.out.println("la masa corporal es ");
+		System.out.println("el indice de la masa corporal es");
+		System.out.println("+calcIMC(peso,altura");
+	
+	}
+
+	public int sumar(int p, int t) {
+
+		return p + t;
+	}
+
+	public double imc(int p,int t) {
+		double d = 1.74;
+		return t * p/t ;
+
+	} 
+
+}
